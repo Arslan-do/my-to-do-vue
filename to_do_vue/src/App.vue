61901eebeaaeb7f3d19e1d87edf1d47ed976b1da
Arslan <blackdemon007@mail.ru>
2026-04-17 23:31:09 +0300

    first commit

---
+++ to_do_vue/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+
+</script>
+
+<template>
+<div>
+<div >
+    <input 
+    placeholder="Add a new task"/>
+    <button >+</button>
+
+    <div> 
+        <h3>Tasks to do -  </h3>
+        <div>
+            <input type="checkbox">
+        <span></span>
+        <button 
+        >🗑</button>
+    </div>
+    </div>
+
+    <div>
+<h3>Done - </h3>
+<div> 
+    <input type="checkbox">
+    <span></span>
+    <button>🗑</button>
+</div>
+    </div>
+</div>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
