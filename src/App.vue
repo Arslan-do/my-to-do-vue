b2d395c3d253456a00bdb1e97260f330a0441fc9
Arslan <blackdemon007@mail.ru>
2026-04-22 23:34:39 +0300

    fix to_do_task-1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref, computed } from 'vue'
 
 const tasks = ref([
   { id: 1, text: 'Купить хлеб и изучить Vue.js', completed: true },
-  { id: 2, text: 'Купить хлеб и изучить Vue.js', completed: false },
+  { id: 2, text: 'Купить продуктов и изучить Vue.js', completed: false },
   { id: 3, text: 'Купить хлеб и изучить Vue.js', completed: false },
   { id: 4, text: 'Купить хлеб и изучить Vue.js', completed: false },
   { id: 5, text: 'To study React fundamentals', completed: false },
@@ -41,7 +41,7 @@ const deleteTask = (id) => {
     placeholder="Add a new task"
     />
     <button @click="addTask" class="add-btn">+</button>
-
+</div>
     <div class="tasks-section"> 
         <h3>Tasks to do - {{ todoTasks.length }} </h3>
         <div v-for="task in todoTasks" 
@@ -52,12 +52,14 @@ const deleteTask = (id) => {
             v-model="task.completed"
             />
         <button @click="deleteTask(task.id)" class="delete-btn"
-        >🗑</button>
+        >
+      <img src="/src\image\TrashSimple.svg" alt="">
+      </button>
     </div>
     </div>
 
-    <div class="done-section">
-<h3>Done -{{ doneTasks.length }}</h3>
+    <div class="done-section" >
+<h3>Done <span v-if="doneTasks.length > 0">- {{ doneTasks.length }}</span></h3>
 <div v-for="task in doneTasks" 
         :key="task.id" 
         class="task-item"
@@ -66,9 +68,14 @@ const deleteTask = (id) => {
     <input type="checkbox"
     v-model="task.completed"
     />
-    <button @click="deleteTask(task.id)" class="delete-btn">🗑</button>
-</div>
-    </div>
+    <button @click="deleteTask(task.id)" class="delete-btn">
+    <img src="/src\image\TrashSimple.svg" alt="нет картины">
+    </button>
+  </div>
+
+  <div v-if="doneTasks.length === 0" class="empty-state">
+      Ничего не найдено...
+  </div>
 </div>
 </div>
 
@@ -77,7 +84,8 @@ const deleteTask = (id) => {
 <style scoped>
 
 .todo-app {
-  max-width: 500px;
+  max-width: 585px;
+  max-height: 758px;
   margin: 0 auto;
   font-family: sans-serif;
   color: #ffff;
@@ -86,7 +94,7 @@ const deleteTask = (id) => {
 .add-task {
   display: flex;
   gap: 8px;
-  flex-flow: column;
+  flex-flow: row;
   margin-bottom: 24px;
 }
 
@@ -98,6 +106,8 @@ color: #ffff;
 }
 
 .add-task input {
+  max-width: none;
+  width: 100%;
   padding: 8px;
   border: 1px solid #9E78CF;
   border-radius: 4px;
@@ -114,6 +124,8 @@ background: #15101C;
 }
 
 .task-item span {
+  width: 227px;
+height: 19px;
     color: #9E78CF;
   flex: 1;
 }
@@ -127,8 +139,13 @@ background: #15101C;
   text-decoration: line-through;
   color: #78CFB0;
 }
+.done-section .task-item span {
+  color: #78CFB0;
+}
 
 .delete-btn {
+width: 30px;
+height: 30px;
   background: none;
   border: none;
   cursor: pointer;
@@ -136,6 +153,21 @@ background: #15101C;
 }
 
 h3 {
-  margin: 16px 0 8px;
+margin: 16px 0 8px;
+width: 115px;
+height: 19px;
+font-family: Inter;
+font-weight: 400;
+font-size: 16px;
+line-height: 100%;
 }
+
+ .task-item input[type="checkbox"] {
+  accent-color: #9E78CF; 
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+
+
 </style>
