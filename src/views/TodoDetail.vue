9f71fcb8526e688ec6ba219ecc3df703755df78d
Arslan <blackdemon007@mail.ru>
2026-05-06 00:58:07 +0300

    fix_api

--- src/views/TodoDetail.vue
+++ src/views/TodoDetail.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { getTodo } from '@/api/todo/getTodo';
-import { reactive, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const todo = reactive({})
+const todo = ref(null);
+
 
 onMounted( async ()=> {
   const newTodo =  await getTodo(route.params.id);
-  Object.assign(todo, newTodo);
+  todo.value = newTodo;
 });
 
 </script>
@@ -16,7 +17,8 @@ onMounted( async ()=> {
 <template>
   <div class="todo-app">
     <div class="container">
-      {{todo}}
+      <div v-if="todo">{{ todo }}</div>
+    <div v-else></div>
     </div>
   </div>
 </template>
